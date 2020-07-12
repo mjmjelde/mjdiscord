1cbe4f2cd9b542c359a991d79b6c75d5f9c8a131
Matthew Mjelde <mjmjelde@gmail.com>
2020-07-11 20:04:32 -0500

    Updated giphy command

--- src/command/giphy_command.ts
+++ src/command/giphy_command.ts
@@ -3,6 +3,7 @@ import { AbstractCommand } from './abstract_command';
 import * as config from 'config';
 import * as giphyapi from 'giphy-api';
 import { commandCharacter, getCommand } from '../util/command';
+import { CommandArgs } from '../util/command_args';
 
 export class GiphyCommand implements AbstractCommand {
 
@@ -21,18 +22,19 @@ export class GiphyCommand implements AbstractCommand {
     }
 
     execute(msg: discord.Message) {
-        let split = msg.content.split(" ");
-        if(split.length < 1){
+        const args = new CommandArgs(msg.content);
+        args.pop(); // pop off the command arg
+        if(args.atEnd()){
             return msg.reply("Invalid usage...\n" + this.help() );
         }
-        let keyword = split[1];
+        const keyword = args.restToString();
         this.giphy.random(keyword).then(image => {
-            if(image.data.url){
+            if (image.data.url){
                 msg.channel.send(image.data.url);
             } else {
                 msg.reply(`no gifs found for ${keyword}`);
             }
-        })
+        });
     }
 
 }
