6c5e02934f1977aa88b142ee09225c0b68fbcac7
Matthew Mjelde <mjmjelde@gmail.com>
2022-11-30 21:49:12 -0600

    Updated random comic command

--- src/commands/randomcomic_command.ts
+++ src/commands/randomcomic_command.ts
@@ -28,30 +28,32 @@ export class RandomComicCommand extends AbstractCommand {
 
   async execute(interaction: CommandInteraction): Promise<void> {
     await interaction.deferReply();
-    axios('http://explosm.net/rcg/view/').then(async (resp) => {
-          const html = parse(resp.data);
-          const imgs = html.querySelector('div.rcg-panels').querySelectorAll('img');
-          const img1 = sharp(await (await axios({ url: imgs[0].attributes.src, responseType: 'arraybuffer' })).data);
-          const img2 = sharp(await (await axios({ url: imgs[1].attributes.src, responseType: 'arraybuffer' })).data);
-          const img3 = sharp(await (await axios({ url: imgs[2].attributes.src, responseType: 'arraybuffer' })).data);
-          const img1Meta = await img1.metadata();
-          const newImg = sharp({create: {width: img1Meta.width * 3, height: img1Meta.height, channels: 4, background: {r: 0, g: 0, b: 0, alpha: 0}}});
-          newImg.composite([
-            { input: await img1.toBuffer(), left: 0, top: 0 },
-            { input: await img2.toBuffer(), left: img1Meta.width, top: 0 },
-            { input: await img3.toBuffer(), left: img1Meta.width * 2, top: 0 }
-          ]).withMetadata().webp( { quality: 90 });
+    axios('https://explosm.net/api/get-random-panels').then(async (resp) => {
+      const jsonResp = resp.data;
+      const img1URL = `https://rcg-cdn.explosm.net/panels/${jsonResp.panels[0].filename}`
+      const img2URL = `https://rcg-cdn.explosm.net/panels/${jsonResp.panels[1].filename}`
+      const img3URL = `https://rcg-cdn.explosm.net/panels/${jsonResp.panels[2].filename}`
+      const img1 = sharp((await axios({ url: img1URL, responseType: 'arraybuffer'})).data);
+      const img2 = sharp((await axios({ url: img2URL, responseType: 'arraybuffer'})).data);
+      const img3 = sharp((await axios({ url: img3URL, responseType: 'arraybuffer'})).data);
+      const img1Meta = await img1.metadata();
+      const newImg = sharp({create: {width: img1Meta.width * 3, height: img1Meta.height, channels: 4, background: {r: 0, g: 0, b: 0, alpha: 0}}});
+      newImg.composite([
+        { input: await img1.toBuffer(), left: 0, top: 0 },
+        { input: await img2.toBuffer(), left: img1Meta.width, top: 0 },
+        { input: await img3.toBuffer(), left: img1Meta.width * 2, top: 0 }
+      ]).withMetadata().webp( { quality: 90 });
 
-          const attachment = new AttachmentBuilder(await newImg.toBuffer());
-          await interaction.editReply({files: [attachment]});
-          newImg.destroy();
-          img1.destroy();
-          img2.destroy();
-          img3.destroy();
-        }).catch(async (err) => {
-          log.error(err);
-          await interaction.editReply("There was an error making your random comic... try again later");
-        });
+      const attachment = new AttachmentBuilder(await newImg.toBuffer());
+      await interaction.editReply({files: [attachment]});
+      newImg.destroy();
+      img1.destroy();
+      img2.destroy();
+      img3.destroy();
+    }).catch(async (err) => {
+      log.error(err);
+      await interaction.editReply("There was an error making your random comic... try again later");
+    });
   }
 
 }
